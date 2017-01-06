3f1d76d8a55873dc56b2b6d246b955810d31dc31
Simon Brouard <sbrouard@users.noreply.github.com>
2017-01-06 20:50:22 +0000

    tests if

--- fusion/tests/test.c
+++ fusion/tests/test.c
@@ -9,5 +9,14 @@ int main(){
   d = -c;
   d = -55.447;
 
+
+  a = a + a;
+  b = b * b;
+  //c = c - c + 32;  // rajouter cast lors de l'addition
+  d = d / d - 32.8;
+
+  a = (a<b);
+
+
   return 0;
 }
---
+++ fusion/tests/test_if.c
@@ -0,0 +1,23 @@
+int main(){
+
+  int a;
+  int b;
+  int c;
+  a = 0;
+  b = 1;
+  c = 0;
+
+  if(a){
+    c++;
+  }
+
+  if(a < b){
+    c++;
+  }
+  else{
+    c--;
+  }
+
+
+  return 0;
+}
